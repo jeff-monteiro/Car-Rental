9f916c5238e31a2df088b4768a575a770166fcda
Jefferson Monteiro <81328013+jeff-monteiro@users.noreply.github.com>
2025-04-15 13:55:34 -0300

    chore: implementation-without-use-interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package src.application;
 
 import src.model.entities.CarRental;
 import src.model.entities.Vehicle;
+import src.model.services.BrazilTaxService;
+import src.model.services.RentalService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -23,6 +25,19 @@ public class Program {
         LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 
         CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+        System.out.print("Enter with price per hour: ");
+        Double pricePerHour = sc.nextDouble();
+        System.out.print("Enter with price per day: ");
+        Double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+        rentalService.processInvoice(cr);
+
+        System.out.println("INVOICE:");
+        System.out.println("Basic Payment: " + cr.getInvoice().getBasicPayment());
+        System.out.println("Tax: " + cr.getInvoice().getTax());
+        System.out.println("Total Payment: " + cr.getInvoice().getTotalPayment());
 
         sc.close();
     }
